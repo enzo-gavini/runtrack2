f58951e9761b2c46dcb298a9269cfea4ad773106
enzo-gavini <enzo.gavini@laplateforme.io>
2025-07-07 16:49:05 +0200

    Exercice 1 of php i use many type of var

---
+++ jour01/job01/index.php
@@ -0,0 +1,12 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+$val = 6;
+$myBool = true;
+echo $str2 . " " . $str . $str3;
+echo " " . $val . " " . ($val+4);
+var_dump($myBool);
+$myBool = false;
+var_dump($myBool);
+?>

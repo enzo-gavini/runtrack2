ddc1ff0442ad0ca3f1bce519f8021f8b56ca2c74
enzo-gavini <enzo.gavini@laplateforme.io>
2025-07-08 14:11:08 +0200

    Using again a loop while and say don't show certain number

---
+++ jour02/job02/index.php
@@ -0,0 +1,31 @@
+<?php
+$x = 0; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job02</title>
+</head>
+<body>
+    <?php 
+    while($x < 1337) {
+    if ($x == 26) {
+        echo "<br />";
+    } elseif ($x == 37) {
+        echo "<br />";
+    } elseif ($x == 88) {
+        echo "<br />";
+    } elseif ($x == 1111) {
+        echo "<br />";
+    } elseif ($x == 3233) {
+        echo "<br />";
+    } else {
+        echo "$x<br />";
+    }
+    $x++;
+    }
+    ?>
+</body>
+</html>

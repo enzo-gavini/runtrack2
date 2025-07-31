da979813451f4ea8502b192ea7cad5892fb666a3
enzo-gavini <enzo.gavini@laplateforme.io>
2025-08-01 00:32:07 +0200

    Modification

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,9 +1,13 @@
 <?php 
 $str="Tous ces intants seront perdus dans le temps comme les larmes sous la pluie.";
-for ($i = 0; $i < strlen($str); $i++) {
-    if ($i % 2 == 0){
-        echo $str[$i];
-    }
-}
-
+// for ($i = 0; $i < strlen($str); $i++) {
+//      if ($i % 2 == 0){
+//         echo $str[$i];
+//      }
+//  }
+ $i = 0;
+ while (isset($str[$i])) {
+     echo $str[$i];
+     $i += 2;
+ }
 ?>

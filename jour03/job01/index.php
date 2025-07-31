4898817cce8a49fe5295bda0a718e11bd395abf9
enzo-gavini <enzo.gavini@laplateforme.io>
2025-07-31 22:29:31 +0200

    Job1 foreach

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,6 +1,12 @@
 <?php
 $entier = [200, 204, 173, 98, 171, 404, 459];
 
-for
+foreach ($entier as $number) {
+    if ($number % 2 == 0) {
+        echo $number , "Nombre pair<br>";
+    } else {
+        echo $number , "Nombre impair<br>";
+    }
+}
 
 ?>

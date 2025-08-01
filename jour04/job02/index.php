f3d40d1fde225131ddee8506ded711baf4cf5847
enzo-gavini <enzo.gavini@laplateforme.io>
2025-08-01 14:01:07 +0200

    Job02

---
+++ jour04/job02/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulaire GET</title>
+</head>
+<body>
+  <h2>Formulaire (méthode GET)</h2>
+  <form method="get" action="">
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom"><br><br>
+
+    <label for="prenom">Prénom :</label>
+    <input type="text" name="prenom" id="prenom"><br><br>
+
+    <label for="email">Email :</label>
+    <input type="text" name="email" id="email"><br><br>
+
+    <label for="ville">Ville :</label>
+    <input type="text" name="ville" id="ville"><br><br>
+
+    <input type="submit" value="Envoyer">
+  </form>
+</body>
+</html>
+<?php
+if (isset($_GET) && !empty($_GET)) {
+  // Ouvre le tableau HTML
+  echo "<h3>Résultats du formulaire :</h3>";
+  echo "<table border='1'>";
+  echo "<tr><th>Argument</th><th>Valeur</th></tr>";
+
+  foreach ($_GET as $argument => $valeur) {
+    echo "<tr>";
+    echo "<td>" . $argument . "</td>";
+    echo "<td>" . $valeur . "</td>";
+    echo "</tr>";
+  }
+
+  echo "</table>";
+}
+?>
